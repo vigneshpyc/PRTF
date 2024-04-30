56a901e1313956aafdbfe88d3453268c4f452c1d
vigneshpyc <145823761+vigneshpyc@users.noreply.github.com>
2024-04-30 22:58:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function navbar(){
+    document.getElementById("nav-bar2").style.display = "block";
+    document.getElementById("navimg").style.display = "none";
+    document.getElementById("cross").style.display = "block";
+
+}
+function cancel(){
+    document.getElementById("nav-bar2").style.display = "none";
+    document.getElementById("navimg").style.display = "block";
+    document.getElementById("cross").style.display = "none" ;
+}
